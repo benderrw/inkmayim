33305335b90ae97ffdf6717d5880b2304f3e9df4
benderrw <benderrw@gmail.com>
2024-12-31 01:04:55 -0300

    Remove Poppins font import and usage
    - Comment out the import of 'poppins' from './assets/fonts'
    - Remove the Poppins font class from the main div element

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { poppins } from './assets/fonts'
+// import { poppins } from './assets/fonts'
 
 export default function Home() {
 	return (
@@ -18,7 +18,7 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="icon" href="/favicon/favicon.ico" />
 			</Head>
-			<div className={`${poppins.className} antialiased`}>
+			<div className={`antialiased`}>
 				<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 					<main className="flex flex-col gap-8 row-start-2 items-center">
 						<a
