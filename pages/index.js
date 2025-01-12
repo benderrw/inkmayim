b4aa6fecb6d23d7947679b219da797970cdc8520
benderrw <benderrw@gmail.com>
2025-01-12 00:34:37 -0300

    Add favicon and improve SEO metadata
    - Add favicon links in _document.js for various sizes and devices
    - Include a web app manifest link
    - Add Head component to index.js with title and meta tags
    - Improve SEO with descriptive title, description, and Open Graph tags
    - Set language attribute to "pt-br" in Html tag

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
@@ -8,25 +9,49 @@ const interSans = Inter({
 
 export default function Home() {
 	return (
-		<div
-			className={`${interSans.variable} font-[family-name:var(--font-inter-sans)] flex justify-center min-h-screen`}
-		>
-			<main className="flex flex-col justify-center">
-				<a
-					href="https://www.instagram.com/inkmayim/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<Image
-						className="dark:invert"
-						src="/svgs/inkmayim.svg"
-						alt="Inkmayim logo"
-						width={180}
-						height={38}
-						priority
-					/>
-				</a>
-			</main>
-		</div>
+		<>
+			<Head>
+				<title>
+					inkmayim | Arte • Tattoo • Piercing • Numerologia • Instrumentos
+					Xamânicos
+				</title>
+				<meta
+					name="description"
+					content="inkmayim oferece tatuagens artísticas, body piercing, numerologia e venda de instrumentos xamânicos. Conecte-se com símbolos culturais e espirituais através de flashes exclusivos, leituras personalizadas e itens únicos de espiritualidade e autoconhecimento."
+				/>
+				<meta
+					property="og:title"
+					content="inkmayim | Arte • Tattoo • Piercing • Numerologia • Instrumentos
+					Xamânicos"
+				/>
+				<meta
+					property="og:description"
+					content="inkmayim oferece tatuagens artísticas, body piercing, numerologia e venda de instrumentos xamânicos. Conecte-se com símbolos culturais e espirituais através de flashes exclusivos, leituras personalizadas e itens únicos de espiritualidade e autoconhecimento."
+				/>
+
+				<meta property="og:url" content="https://www.inkmayim.com.br" />
+				<meta property="og:type" content="website" />
+			</Head>
+			<div
+				className={`${interSans.variable} font-[family-name:var(--font-inter-sans)] flex justify-center min-h-screen`}
+			>
+				<main className="flex flex-col justify-center">
+					<a
+						href="https://www.instagram.com/inkmayim/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<Image
+							className="dark:invert"
+							src="/svgs/inkmayim.svg"
+							alt="Inkmayim logo"
+							width={180}
+							height={38}
+							priority
+						/>
+					</a>
+				</main>
+			</div>
+		</>
 	)
 }
